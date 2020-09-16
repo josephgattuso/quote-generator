34a495fada076815531742c1d10e93d63851cbae
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-09-16 08:17:03 +0100

    add quotes api

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+// Get Quote from API
+async function getQuote() {
+  const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+  const apiUrl =
+    'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en';
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    getQuote();
+    console.log('whoops, no quote', error);
+  }
+}
+
+// On Load
+getQuote();
