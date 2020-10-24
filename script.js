27d7c7f4cbe50ed04377c21688da220d97929e0c
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-10-24 03:21:52 +0100

    update quote dynamically using api

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quote from API
 async function getQuote() {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
@@ -6,7 +12,8 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    authorText.innerText = data.quoteAuthor;
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     getQuote();
     console.log('whoops, no quote', error);
