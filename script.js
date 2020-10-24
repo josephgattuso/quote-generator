fe962d707190385d0d77021be1acc703be96cfba
Joseph Gattuso <joseph.gattuso@gmail.com>
2020-10-24 07:48:34 +0100

    refactor get quotes from api

--- script.js
+++ script.js
@@ -4,19 +4,30 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 
-// Get Quote from API
+// Get Quote From API
 async function getQuote() {
+  // We need to use a Proxy URL to make our API call in order to avoid a CORS error
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
   const apiUrl =
-    'http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en';
+    'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    authorText.innerText = data.quoteAuthor;
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (data.quoteAuthor === '') {
+      authorText.innerText = 'Unknown';
+    } else {
+      authorText.innerText = data.quoteAuthor;
+    }
+    // Dynamically reduce font size for long quotes
+    if (data.quoteText.length > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
     quoteText.innerText = data.quoteText;
   } catch (error) {
     getQuote();
-    console.log('whoops, no quote', error);
   }
 }
 
